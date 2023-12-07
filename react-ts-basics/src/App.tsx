ddbb74071c82cf014f839f23f138172e50488f7a
Anil Acar <harunanil.acar@publicisgroupe.cz>
2023-12-07 22:46:07 +0100

    first component done

---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,11 @@
+import CourseGoal from './components/CourseGoal.tsx'
+
+function App() {
+  return (
+    <main>
+      <CourseGoal title='Ts+React' description='TS REACT course' />
+    </main>
+  )
+}
+
+export default App
