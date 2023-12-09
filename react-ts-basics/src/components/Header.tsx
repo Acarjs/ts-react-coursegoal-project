e40c1c9bead79c7db51002d0492cdb35195c5023
Anil Acar <harunanil.acar@publicisgroupe.cz>
2023-12-09 14:41:22 +0100

    add comment to image spreading

--- react-ts-basics/src/components/Header.tsx
+++ react-ts-basics/src/components/Header.tsx
@@ -12,7 +12,9 @@ const Header: FC<HeaderProps> = ({ image, children }) => {
   return (
     <section>
       {/* <img src={image.src} alt={image.alt} /> */}
-      <img {...image} /> //you can destruct this way
+      <img {...image} /> //you can destruct this way. This is a general REACT
+      shortcut which will simply take all key value pairs in an object and
+      spread them as props, as key value pairs.
       {children}
     </section>
   )
