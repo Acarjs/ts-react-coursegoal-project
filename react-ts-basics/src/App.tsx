f9f1da2aad20a70cc8bf6be225db5e308bdad270
Anil Acar <harunanil.acar@publicisgroupe.cz>
2023-12-10 18:40:15 +0100

    add comment to image spreading_fix

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,16 +1,37 @@
-import CourseGoal from './components/CourseGoal.tsx'
 import Header from './components/Header.tsx'
 import goalImg from './assets/goals.jpg'
+import { useState } from 'react'
+import CourseGoalList from './components/CourseGoalList.tsx'
+
+export type CourseGoal = {
+  title: string
+  description: string
+  id: number
+}
 
 function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([])
+
+  const handleAddGoal = () => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        title: 'Learn React + TS',
+        description: 'Learn it in depth',
+        id: Math.random(),
+      }
+      return [...prevGoals, newGoal]
+    })
+
+    // if your new state depends on the old state, you should use this special function form : ()=>{} for updating the state.
+  }
+
   return (
     <main>
       <Header image={{ src: goalImg, alt: 'your goal image' }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title='Ts+React'>
-        <p>TS REACT course</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <CourseGoalList goals={goals} />
     </main>
   )
 }
--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,7 +1,13 @@
 //----------------------SYNTAX 1--------------
-import { type PropsWithChildren } from 'react'
+// import { type PropsWithChildren } from 'react'
+import { ReactNode } from 'react'
 
-type CourseGoalProps = PropsWithChildren<{ title: string }>
+// type CourseGoalProps = PropsWithChildren<{ title: string }>
+
+type CourseGoalProps = {
+  title: string
+  children: ReactNode
+}
 
 export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
